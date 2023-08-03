5e55293c396e0fe750ae2f137d19ee57686a1528
al.tukanov@gmail.com <al.tukanov@gmail.com>
2023-08-03 20:23:08 +0400

    hints added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a: ");
         int a = scn.nextInt();
+        System.out.println("Enter b: ");
         int b = scn.nextInt();
         System.out.println(a+b);
 
